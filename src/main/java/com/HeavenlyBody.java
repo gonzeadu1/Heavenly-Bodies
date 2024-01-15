e675c9d265104be8a49e4af9d8fcb612612a0c4a
Gideon Nzeadu <gnzeadu@yahoo.com>
2024-01-15 17:15:46 -0600

    Creating different HeavenlyBodies

---
+++ src/main/java/com/HeavenlyBody.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return satellites;
+    }
+
+
+}
